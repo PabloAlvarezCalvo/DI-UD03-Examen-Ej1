232b30d07cf7032317e82fcc87ab635d557380b4
Pablo Álvarez Calvo <a21pabloac1@ateis.local>
2023-03-15 16:55:53 +0100

    Correccion carga css images

---
+++ src/main/java/com/example/examen_ejercicio1/vistaPrincipalController.java
@@ -0,0 +1,75 @@
+package com.example.examen_ejercicio1;
+
+import com.example.examen_ejercicio1.models.Ciudad;
+import javafx.beans.InvalidationListener;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+
+import java.net.URL;
+import java.util.*;
+
+public class vistaPrincipalController implements Initializable {
+    @FXML
+    private ListView <Ciudad> listaVistaCiudades;
+    @FXML
+    private ImageView imagenCiudades;
+
+    @FXML Button btnAsignarEstilo, btnLimpiarEstilo;
+
+    @FXML Label lblTitulo;
+
+    @FXML
+    private final ObservableList <Ciudad> listaCiudades =
+           FXCollections.observableArrayList();
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        listaCiudades.add(new Ciudad("Madrid", getClass().getResource("images/img1.jpg").toExternalForm()));
+        listaCiudades.add(new Ciudad("Barcelona", getClass().getResource("images/img2.jpg").toExternalForm()));
+        listaCiudades.add(new Ciudad("Sevilla", getClass().getResource("images/img3.jpg").toExternalForm()));
+        listaCiudades.add(new Ciudad("Málaga", getClass().getResource("images/img4.jpg").toExternalForm()));
+        listaCiudades.add(new Ciudad("Valencia", getClass().getResource("images/img5.jpg").toExternalForm()));
+        listaCiudades.add(new Ciudad("Oporto", getClass().getResource("images/img6.jpg").toExternalForm()));
+
+
+    // Vamos a asignar la lista observable a la lista
+        listaVistaCiudades.setItems(listaCiudades);
+    //Debes modificar el ListView para que funcione con la clase Ciudad.
+        listaVistaCiudades
+                .getSelectionModel()
+                .selectedItemProperty()
+                .addListener(new ChangeListener<Ciudad>() {
+            @Override
+            public void changed(ObservableValue<? extends Ciudad> observableValue, Ciudad oldCiudad, Ciudad newCiudad) {
+                System.out.println (newCiudad.getNombre() );
+                imagenCiudades.setImage(new Image(newCiudad.getUrl()));
+                imagenCiudades.preserveRatioProperty();
+            }
+        });
+    }
+
+    @FXML private void btnAsignarEstiloOnClick(){
+        btnAsignarEstilo.getScene().getStylesheets().add(getClass().getResource("css/estilo.css").toExternalForm());
+    }
+
+    @FXML private void btnLimpiarEstiloOnClick(){
+        btnLimpiarEstilo.getScene().getStylesheets().clear();
+    }
+}
